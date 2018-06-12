05cae6cb5f0bff6f788036bfbdf3fb763d58ae92
Mike Antonelli <michael.antonelli@cerner.com>
2018-06-12 13:43:37 -0500

    WIP: Add database integration (MySQL).

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,12 @@ class WelcomeController < ApplicationController
 
   def index
     @version_props = {
+      dbAdapter: ActiveRecord::Base.connection_config[:adapter],
+      dbHost: ActiveRecord::Base.connection_config[:host],
+      dbMigrationVersion: ActiveRecord::Migrator.current_version.to_s,
       railsVersion:  Rails.version,
       rubyPlatform: RUBY_PLATFORM,
-      rubyVersion: RUBY_VERSION
+      rubyVersion: RUBY_VERSION,
     }
 
     Rails.logger.info "#{self.class.name}: Preparing to render with props: #{@version_props}"
